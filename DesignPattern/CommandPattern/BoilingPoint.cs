07087735bff1a30fab56e6781b229d0d303fb8a8
vikas <vikasjkitawat@gmail.com>
2017-06-07 12:21:45 +0530

    Command Pattern

---
+++ DesignPattern/CommandPattern/BoilingPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class BoilingPoint
+    {
+        public event EventHandler OnBoiled;
+
+        public void BoilWater()
+        {
+            for (int i = 0; i <= 150; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Water Temp is " + i);
+                if (i == 100 && OnBoiled!= null)
+                {
+                    OnBoiled?.Invoke(null,null);
+                }
+            }
+        }
+    }
+}
---
+++ DesignPattern/CommandPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoilingPoint plant = new BoilingPoint();
+            
+            plant.OnBoiled += MakeSound;
+            plant.BoilWater();
+            plant.OnBoiled += PopUp;
+            plant.BoilWater();
+            plant.OnBoiled += ShutDown;
+            plant.BoilWater();
+            Console.ReadKey();
+        }
+
+        private static void ShutDown(object sender, EventArgs e)
+        {
+            Console.WriteLine("Shutting down Machine");
+        }
+
+        private static void PopUp(object sender, EventArgs e)
+        {
+            Console.WriteLine("Water is boiled, Click popup to close...");
+        }
+
+        private static void MakeSound(object sender, EventArgs e)
+        {
+            Console.WriteLine("Water is boiled, Beep...Beep....Beep...");
+        }
+    }
+}
